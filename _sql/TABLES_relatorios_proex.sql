91782e885cf05facdb089a7cb5c4f89a25ce9e89
Pedro <lopes.pedro@aluno.ufca.edu.br>
2024-03-15 11:17:37 -0300

    organizing our sql files and starting dashboard page

--- _sql/TABLES_relatorios_proex.sql
+++ _sql/TABLES_relatorios_proex.sql
@@ -177,34 +177,3 @@ CREATE TABLE IF NOT EXISTS `permissoes` (
   `dash_ordens_judiciais` int(11) NOT NULL DEFAULT 0,
   `dash_central_de_marcacoes` int(11) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=latin1;
-
--- COLOCAR INSERTS AQUI EM BAIXO
-
---
--- Inserindo usuario
---
-
-SELECT * from `usuarios`;
-INSERT INTO `usuarios` (`usuarioID`, `nomeUsuario`, `siapeUsuario`, `cpfUsuario`, `loginUsuario`, `senhaUsuario`, `tipoUsuario`, `statusUsuario`, `unidadeAcademicaUsuario`, `registroUsuario_ID`, `dataRegistroUsuario`) VALUES
-(1, "Dorgival Netto", "40028922", 1, "dorgival.netto@ufca.edu.br", 'MTIzNDU2', 1, 1, 2, 1, '2023-01-05 09:24:59');
-
---
--- Inserindo permissoes
---
-
-SELECT * from `permissoes`;
-INSERT INTO `permissoes` (`idPermissao`, `usuarios`, `getIdUsu`, `configuracoes_prefeituras`, `configuracoes_secretarias`, `configuracoes_diretorias`, `configuracoes_estrutura`, `configuracoes_servidores`, `configuracoes_cargos`, `configuracoes_lotacoes`, `configuracoes_vinculos`, `questionarios_colaboradores`, `processos_inserir`, `processos_stages`, `processos_editar`, `formularios_almoxarifado`, `formularios_compras`, `formularios_ordens_judiciais`, `formularios_vigencia_contratos`, `formularios_registros_beneficios`, `formularios_enel`, `formularios_folha_pagamento`, `processos_powerbi`, `dash_diagnostico_sesau`, `dash_enel`, `dash_folha_de_pagamento`, `dash_atencao_primaria`, `dash_beneficios`, `dash_vigencia_de_contratos`, `dash_combustiveis`, `dash_ordens_judiciais`, `dash_central_de_marcacoes`) VALUES
-(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1);
-
---
--- Fazendo dump de dados para tabela `unidadeAcademica`
---
-
-SELECT * from `unidadeAcademica`;
-INSERT INTO `unidadeAcademica` VALUES (1, 'Centro de Ciências Agrárias e da Biodiversidade (CCAB)'),
-(2, 'Centro de Ciências e Tecnologia (CCT)'),
-(3, 'Centro de Ciências Sociais e Aplicadas (CCSA)'),
-(4, 'Instituto de Estudos do Semiárido (IESA)'),
-(5, 'Instituto de Formação de Educadores (IFE)'),
-(6, 'Instituto Interdisciplinar de Sociedade, Cultura e Artes (IIsca)'),
-(7, 'Faculdade de Medicina (Famed)');
--- common/container.php
+++ common/container.php
@@ -14,38 +14,33 @@
 
   <div class="row">
   <?
-    $idUsu = $_SESSION['usuarioID'];
-    $selectPermissoesPorIdUser = selectPermissoesPorIdUser($idUsu);
-    $lsSelectPermissoesPorIdUser = mysqli_fetch_assoc($selectPermissoesPorIdUser);  
+    $idUsu = $_SESSION['usuarioID']; 
     //$selectPaineisBI = selectPaineisBIAsc();
     //while($ls = mysqli_fetch_object($selectPaineisBI)):
-      if($lsSelectPermissoesPorIdUser[$ls->coluna_permissao] == 1):
-  ?>
-    <div class="col-xl-3 col-md-6 mb-4">
-      <? if($ls->nome_painel == "diagnóstico geral da sesau"): ?>
-        <div class="card border-left-danger shadow h-100 py-2">
-      <? else: ?>
-        <div class="card border-left-primary shadow h-100 py-2">
-      <? endif; ?>
-        <a href="<?= $ls->link_painel ?>" target="_blank">
-          <div class="card-body">
-            <div class="row no-gutters align-items-center">
-              <div class="col mr-2">
-                <div class="text-sm font-weight-bold text-primary text-uppercase mb-1">
-                  <?= $ls->nome_painel ?>
+    ?>
+      <div class="col-xl-3 col-md-6 mb-4">
+        <? if($ls->nome_painel == "Relatório Ação"): ?>
+          <div class="card border-left-danger shadow h-100 py-2">
+        <? else: ?>
+          <div class="card border-left-primary shadow h-100 py-2">
+        <? endif; ?>
+          <a href="<?= $ls->link_painel ?>" target="_blank"> <!-- inserir em _blank a pagina do relatorio -->
+            <div class="card-body">
+              <div class="row no-gutters align-items-center">
+                <div class="col mr-2">
+                  <div class="text-sm font-weight-bold text-primary text-uppercase mb-1">
+                    <?= $ls->nome_painel ?>
+                  </div>
+                </div>
+                <div class="col-auto">
+                  <i class="fas fa-chart-line fa-2x text-gray-300"></i>
                 </div>
               </div>
-              <div class="col-auto">
-                <i class="fas fa-chart-line fa-2x text-gray-300"></i>
-              </div>
             </div>
-          </div>
-        </a>
+          </a>
+        </div>
       </div>
-    </div>
-  <? 
-    endif;
-    //endwhile;
+    <? 
   ?>
 
   </div>
